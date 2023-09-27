8e9f6056f9e3b199203cdeb68d2d6a3902d149ad
OlvMrn <olivier.morin91@gmail.com>
2023-09-27 17:30:50 +0200

    Section restaurants creation fonction qui permet de créer les cartes, et de gérer le CSS lorsqu'on appuie sur les filtres

--- script.js
+++ script.js
@@ -1,23 +1,63 @@
 const allRestaurants = [
+  /* {
+   name: "",
+   picture: "",
+   foodCategory: ""
+   favory: Boolean
+  } */
   {
     name: "Restaurant1",
-    picture: "https://placekitten.com/200/287"
+    picture: "https://placekitten.com/200/287",
+    foodCategory: "",
+    favory: false
   },
   {
     name: "Restaurant2",
-    picture: "https://placekitten.com/200/139"
+    picture: "https://placekitten.com/200/145",
+    foodCategory: "",
+    favory: false
   },
   {
     name: "Restaurant3",
-    picture: "https://placekitten.com/200/90"
+    picture: "https://placekitten.com/200/90",
+    foodCategory: "",
+    favory: false
   },
   {
     name: "Restaurant4",
-    picture: "https://placekitten.com/200/194"
+    picture: "https://placekitten.com/200/194",
+    foodCategory: "",
+    favory: false
   },
   {
     name: "Restaurant5",
-    picture: "https://placekitten.com/200/179"
+    picture: "https://placekitten.com/200/179",
+    foodCategory: "",
+    favory: false
+  },
+  {
+    name: "Restaurant6",
+    picture: "https://placekitten.com/200/164",
+    foodCategory: "",
+    favory: false
+  },
+  {
+    name: "Restaurant7",
+    picture: "https://placekitten.com/200/190",
+    foodCategory: "",
+    favory: false
+  },
+  {
+    name: "Restaurant8",
+    picture: "https://placekitten.com/200/165",
+    foodCategory: "",
+    favory: false
+  },
+  {
+    name: "Restaurant9",
+    picture: "https://placekitten.com/200/122",
+    foodCategory: "",
+    favory: false
   }
 ];
 
@@ -32,6 +72,13 @@ const createRestaurantCard = (imageUrl) => {
 }
 
 for (let i = 0; i < allRestaurants.length; i++) {
-  console.log(i);
   createRestaurantCard(allRestaurants[i].picture);
+}
+
+const filter = document.querySelectorAll(".filter");
+console.log(filter);
+for(let i = 0; i < filter.length; i++){
+  filter[i].addEventListener("click", function () {
+    filter[i].classList.toggle("yellow-background");
+  });
 }
